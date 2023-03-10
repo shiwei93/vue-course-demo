f0852d8f525463ddfd18db606c527ac314683462
shiwei93 <stayfocusjs@gmail.com>
2023-03-10 18:02:04 +0800

    feat: add counter demo

---
+++ counter/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+    <div class="sdfasd">
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+    </div>
+    <div>
+        <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+        <a href="https://vitejs.dev/guide/api-js" target="_blank">API</a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    transition: filter 300ms;
+    will-change: filter;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
