22aae157666570231a89ccb4a433bbae3ae96c07
shiwei93 <stayfocusjs@gmail.com>
2023-03-14 18:18:41 +0800

    feat: add support tailwindcss

--- pokemon-picker-app/src/views/HomeView.vue
+++ pokemon-picker-app/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <main>
-        <TheWelcome />
+        <div class="flex justify-center text-red-300 text-lg font-bold">Hello world</div>
     </main>
 </template>
---
+++ pokemon-picker-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: {
+        content: ['./public/**/*.html', './src/**/*.vue']
+    },
+    content: [],
+    theme: {
+        colors: {},
+        extend: {}
+    },
+    plugins: []
+}
